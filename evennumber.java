f5c632c0862be942405dacf905ff5ed38b7afbbb
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-17 18:32:35 +0530

    even number program

---
+++ evennumber.java
@@ -0,0 +1,10 @@
+public class evennumber{
+  public static void main(String[] args){
+    int num;
+    if(num%2==0){
+      system.out.println("the given number is even number");
+    }
+    else{
+      system.out.println("the given number is odd number");
+    }
+  }
