4e7e35c698f28614e2b6ce6f7b6ec65fb4db49e9
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-18 18:28:28 +0530

    star problem

---
+++ star.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class star{
+    public static void main(String[] args) {
+        char [][] arr={
+        {'*','*','*'},{'*','*','*'},{'*','*','*'}
+    };
+  System.out.println(Arrays.deepToString(arr));
+        }
+    }
