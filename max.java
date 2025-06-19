da83a92e2653dff76cd609a0a06a88bf03f396a9
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-19 16:38:15 +0530

    maximum numbers  from the array

---
+++ max.java
@@ -0,0 +1,15 @@
+public class max {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 55, 67, 90};
+        int max = arr[0]; 
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
+
